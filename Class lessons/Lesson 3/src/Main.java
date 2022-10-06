04e8b3cb945c39734e7de2a34708c7e2f38c98e6
Alexandr Zhivaykin <63651749+Sashkewic@users.noreply.github.com>
2022-10-06 19:48:43 +0600

    Add files via upload

---
+++ Class lessons/Lesson 3/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        // Урок 3 - Ветвления.
+//        int firstNum, secondNum, thirdNum;
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Введите три целых числа: ");
+//        firstNum = scan.nextInt();
+//        secondNum = scan.nextInt();
+//        thirdNum = scan.nextInt();
+//
+//        if (firstNum > secondNum && firstNum > thirdNum) {
+//            System.out.println("Максимальное число: " + firstNum);
+//        }
+//
+//        else if (secondNum > firstNum && secondNum > thirdNum) {
+//            System.out.println("Максимальное число: " + secondNum);
+//        }
+//
+//        else {
+//            System.out.println("Максимальное число: " + thirdNum);
+//        }
+
+        
+    }
+}
