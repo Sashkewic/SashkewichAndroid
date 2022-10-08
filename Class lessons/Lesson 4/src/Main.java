05d9ad48a1e8f79c6cb587a026c991107c147cd9
Alexandr Zhivaykin <63651749+Sashkewic@users.noreply.github.com>
2022-10-08 17:48:52 +0600

    Add files via upload
    Добавлен урок 4 - циклы

---
+++ Class lessons/Lesson 4/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Урок 4. Циклы.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Введите два числа: ");
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        int count = 1;
+//
+//        while (num1 <= num2) {
+//            System.out.println( num1 + " * " + num1 + " = " + Math.pow(num1, 2));
+//            num1 = num1 + count;
+//        }
+
+
+
+
+
+
+
+
+
+//        int num = sc.nextInt();
+//        int count = 0;
+//
+//        while (num > 0) {
+//            num = num / 10;
+//            count++;
+//        }
+//
+//        System.out.printf("Количество цифр в числе: %d", count);
+
+
+    }
+}
